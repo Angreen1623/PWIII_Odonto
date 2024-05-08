37906aa095443c58ff76cf289e8639e10da3e999
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-05-08 14:14:10 -0300

    .

--- resources/views/Cadastrar.blade.php
+++ resources/views/Cadastrar.blade.php
@@ -21,26 +21,12 @@
         </style>
     </head>
     <body class="antialiased">
-        <?php view('navbar');?>
-
+       
     <div class="container">
-        <div class="col">
-            <div class="row">
-                <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body mb-3" data-bs-theme="dark">">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Sistema de Agendamento de consultas</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav">
-                                <a class="nav-link active" aria-current="page" href="/">Cadastrar</a>
-                                <a class="nav-link" href="/listar">Consultar</a>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+        
+         <?php view('navbar');?>
+        
+         <div class="col">
             <div class="row">
                 <p>
                 <h3>Sistema de um Consultório Odontológico</h3>  
