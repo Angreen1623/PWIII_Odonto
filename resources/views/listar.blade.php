d9fe06356bcf38eb630becad9308e30423f2b02c
IgorNogueiraPessoa.com <igornnpessoa@gmail.com>
2024-05-15 11:14:17 -0300

    Fazendo Listar

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -8,33 +8,47 @@
     <title>Listar</title>
 </head>
 
-<body><?php view('navbar');?>
-  
+<body>
+    <?php view('navbar'); ?>
 
-<main class="grid min-h-screen w-full place-items-center bg-gray-900">
-    <div class="absolute left-1/2 top-1/2 h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-6 rounded-2xl bg-gray-400"></div>
+    <section class="antialiased bg-gray-50 text-gray-600 min-h-screen p-4">
 
-    <div class="absolute left-1/2 top-1/2 h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-6 space-y-6 rounded-2xl bg-gray-100 p-6 transition duration-300 hover:rotate-0">
-        <div class="flex justify-end">
-            <div class="h-4 w-4 rounded-full bg-gray-900"></div>
-        </div>
+        <h3 class="text-3xl">Consultas agendadas</h3>
 
-        <header class="text-center text-xl font-extrabold text-gray-600">2021.09.01</header>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            @foreach($paciente as $pac)
+            <!-- Card -->
+            <div class="h-200 w-200">
+                <div class="relative bg-indigo-700 shadow-lg rounded-lg shadow-lg p-5 overflow-hidden">
 
-        <div>
-            <p class="text-center text-5xl font-extrabold text-gray-900">Online Test (Physics)</p>
-            <p class="text-center text-4xl font-extrabold text-[#FE5401]">2 hours</p>
-        </div>
+                    <!-- Card content -->
+                    <div class="relative pt-[6.25rem] pb-14">
+                        <div class="text-xs font-bold uppercase text-green-400 tracking-widest mb-2">{{ $pac->nome_pac }}</div>
+                        <h3 class="text-2xl font-extrabold text-indigo-50 leading-snug mb-2">The easy way to scan your documents</h3>
+                        <p class="text-indigo-200">Lorem ipsum dolor sit amet, consecte adipiscing elit sed do eiusmod.</p>
+                    </div>
+                    <!-- Card footer -->
+                    <div class="relative text-right">
+                        <button class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                            Deletar
+                        </button>
 
-        <footer class="mb-10 flex justify-center">
-            <button class="flex items-baseline gap-2 rounded-lg bg-[#FE5401] px-4 py-2.5 text-xl font-bold text-white hover:bg-[#FF7308]">
-                <span>Start</span>
-                <i class="fas fa-hand-peace text-xl"></i>
-            </button>
-        </footer>
-    </div>
-</main>
+                            <button class="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                                </svg>
 
+                                Alterar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+    </section>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::post("/adicionar", [PacienteController::class, "adicionar"]);
 Route::post("/", [PacienteController::class, "atualizar"]);
 Route::get("/", [PacienteController::class, "excluir"]);
 Route::get("/", [PacienteController::class, "listar"]);*/
-=======
+
 Route::post("/", [PacienteController::class, "adicionar"]);
 Route::get("/", [PacienteController::class, "editar"]);
 Route::post("/", [PacienteController::class, "atualizar"]);
 Route::get("/", [PacienteController::class, "excluir"]);
-Route::get("/", [PacienteController::class, "listar"]); */
+Route::get("/listar", [PacienteController::class, "listar"]); 
 
 Route::get("/", function(){
     return view('navbar');
@@ -36,4 +36,4 @@ Route::get("/listar", [PacienteController::class, "listar"]);
 Route::get('/', function (){
     return view('Cadastrar');
 });
->>>>>>> ba7ce74dca3b5237374ed74eea63091e5a5fd3b6
+
