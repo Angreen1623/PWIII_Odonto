7514b8933007837ff75a021afb0ad88af48a91c3
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-05-15 13:54:55 -0300

    bruno

--- resources/views/Cadastrar.blade.php
+++ resources/views/Cadastrar.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://cdn.tailwindcss.com"></script>
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -17,50 +17,42 @@
        
     <div class="container">
 
-       <?php view('navbar');?>
+       @include('navbar')
         
-    <form method="POST" action="/adicionar">
-     @csrf    
-  <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Sistema de um Consultório Odontológico</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">Agendamento de Consultas</p>
-
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nome do Paciente</label>
-                <div class="mt-2">
-                   <input type="text" name="nome_pac"  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Nome do Paciente:">
-                </div>
-            </div>
-         </div>
-
-         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Data de Nascimento</label>
-                <div class="mt-2">
-                   <input type="date" name="dt_nasc" value="dt_nasc" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Data de Nascimento:">
-                </div>
-            </div>
-         </div>
-
-         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nome do Médico</label>
-                <div class="mt-2">
-                   <input type="text" name="nome_med" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Nome do Médico:">
-                </div>
-            </div>
-         </div>
-
+       <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+  <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
+    <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+  </div>
+  <div class="mx-auto max-w-2xl text-center">
+    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Agendamento de Consultas</h2>
+    <p class="mt-2 text-lg leading-8 text-gray-600">Sistema de um Consultório Odontológico</p>
+  </div>
+  <form action="/adicionar" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+    <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+      <div>
+        <label for="nome_pac" class="block text-sm font-semibold leading-6 text-gray-900">Nome do paciente</label>
+        <div class="mt-2.5">
+          <input type="text" name="nome_pac" id="nome_pac" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
       </div>
-     </div>
+      <div>
+        <label for="dt_nasc" class="block text-sm font-semibold leading-6 text-gray-900">Data de Nascimento</label>
+        <div class="mt-2.5">
+          <input type="date" name="dt_nasc" id="dt_nasc" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+      <div class="sm:col-span-2">
+        <label for="nome_med" class="block text-sm font-semibold leading-6 text-gray-900">Nome do Médico</label>
+        <div class="mt-2.5">
+          <input type="text" name="nome_med" id="nome_med" autocomplete="organization" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
 
-        <div class="sm:col-span-3">
-          <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Tipo de Consulta</label>
-          <div class="mt-2">
-            <select id="tipo_consulta" name="tipo_consulta" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+      <div class="sm:col-span-2">
+        <label for="tipo_consulta" class="block text-sm font-semibold leading-6 text-gray-900">Tipo de Consulta</label>
+        <div class="relative mt-2.5">
+          <div class="absolute inset-y-0 left-0 flex items-center">
+          <select id="tipo_consulta" name="tipo_consulta" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             <option value="nenhum foi selecionado">Selecionar</option>
                         <option value="Cirurgia">Cirurgia</option>
                         <option value="Checkup">Checkup</option>
@@ -70,38 +62,34 @@
             </select>
           </div>
         </div>
+      </div>
 
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Data da Consulta</label>
-                <div class="mt-2">
-                   <input type="date" name="data" value="data" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Data da Consulta:">
-                </div>
-            </div>
-         </div>
-
-         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Hora da Consulta</label>
-                <div class="mt-2">
-                   <input type="time" name="hora" value="hora" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Hora da Consulta:">
-                </div>
-            </div>
-         </div>
-
-         <div class="col-span-full">
-            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Observação</label>
-            <div class="mt-2">
-                <textarea id="about" name="observacao" rows="3" value="observacao" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-            </div>
-         </div> 
-  </div>
-
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Agendar</button>
-  </div>
-</form>
+      <div>
+        <label for="data" class="block text-sm font-semibold leading-6 text-gray-900">Data da Consulta</label>
+        <div class="mt-2.5">
+          <input type="date" name="data" id="data" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
+      </div>
+      <div>
+        <label for="hora" class="block text-sm font-semibold leading-6 text-gray-900">Hora de Consulta</label>
+        <div class="mt-2.5">
+          <input type="time" name="hora" id="hora" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <div class="sm:col-span-2">
+        <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Observação</label>
+        <div class="mt-2.5">
+          <textarea name="oservacao" id="observacao" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+        </div>
+      </div>
     </div>
+    <div class="mt-10">
+      <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Agendar</button>
+    </div>
+  </form>
+</div>
+   
+ </div>
 </body>
 </html>
