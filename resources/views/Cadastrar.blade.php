f9fabe78152f70ece6c19ce202b5e06f7d0fabb3
Angreen1623 <andreopaim@gmail.com>
2024-05-15 14:29:29 -0300

    uns treco para deixar belo

--- resources/views/Cadastrar.blade.php
+++ resources/views/Cadastrar.blade.php
@@ -18,7 +18,7 @@
     <div>
        @include('navbar')
         
-       <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+       <div class="isolate bg-white px-6 pt-24 sm:pt-32 lg:px-8">
   <div class="mx-auto max-w-2xl text-center">
     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Agendamento de Consultas</h2>
     <p class="mt-2 text-lg leading-8 text-gray-600">Sistema de um Consultório Odontológico</p>
@@ -95,7 +95,7 @@
 
     <!--botão-->
     <div class="mt-10">
-      <button type="submit" class="block w-full rounded-md bg-indigo-300 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Agendar</button>
+      <button type="submit" class="block w-full rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Agendar</button>
     </div>
 
   </form>
