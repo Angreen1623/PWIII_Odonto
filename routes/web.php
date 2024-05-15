fe3c626fb9f304cf2d414390adda2e26ee0daadf
Angreen1623 <andreopaim@gmail.com>
2024-05-15 14:23:11 -0300

    removendo o debbug do routes

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,6 @@ Route::get("/", [PacienteController::class, "listar"]);*/
 Route::post("/adicionar", [PacienteController::class, "adicionar"]);
 Route::get("/listar", [PacienteController::class, "listar"]);
 
-Route::get("/navbar", function(){
-    return view('navbar');
-});
-
 Route::get('/', function (){
     return view('Cadastrar');
 });
