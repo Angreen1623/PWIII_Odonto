f0c5c161a2506e1a700b6a4bacae1d30289d0d5b
angreen1623 <andreopaim@gmail.com>
2024-09-11 14:14:16 -0300

    criando o seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'name' => 'André',
+             'email' => 'andreopaim@gmail.com',
+             'password' => '$2y$10$SgKY6lTPqz0vTbi7Lr4Fz.fdl7B3ydMSndTo2jyO0UUUvXXkxSz4C',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Igor',
+            'email' => 'igornogueira@gmail.com',
+            'password' => '$2y$10$SgKY6lTPqz0vTbi7Lr4Fz.fdl7B3ydMSndTo2jyO0UUUvXXkxSz4C',
+       ]);
+       
+        \App\Models\User::factory()->create([
+            'name' => 'Bruno',
+            'email' => 'brunoaparecido@gmail.com',
+            'password' => '$2y$10$SgKY6lTPqz0vTbi7Lr4Fz.fdl7B3ydMSndTo2jyO0UUUvXXkxSz4C',
+       ]);
     }
 }
